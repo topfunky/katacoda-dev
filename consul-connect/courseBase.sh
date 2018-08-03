8c4a9be7917bbcca2ed020eb0613e75bc5c5b353
Geoffrey Grosenbach <boss@topfunky.com>
2018-08-03 16:23:29 -0700

    Updated

--- consul-connect/courseBase.sh
+++ consul-connect/courseBase.sh
@@ -1,14 +1,12 @@
 
 host_commands=(
-"mkdir -p ~/.bin"
 "mkdir -p ~/src"
 "mkdir -p ~/tutorial/config"
-"cd ~/src && curl -L http://assets.joinscrapbook.com/unzip -O"
-"chmod +x ~/src/unzip"
-"mv ~/src/unzip /usr/local/bin"
+"cd ~/src && curl -L http://assets.joinscrapbook.com/unzip -o /usr/local/bin/unzip"
+"chmod +x /usr/local/bin/unzip"
 
 "cd ~/src && curl -L https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip -O"
-"unzip -d ~/.bin ~/src/consul*.zip"
+"unzip -d /usr/local/bin ~/src/consul*.zip"
 
 "mkdir -p ~/log"
 "nohup sh -c \"consul agent -dev -config-dir=~/config >~/log/consul.log 2>&1 &\" &"
@@ -22,5 +20,6 @@ do
   $(${cmd})
 done
 
-all_commands=$(join " && " ${host_commands[@]})
+all_commands=$(join && ${host_commands[@]})
+echo "${all_commands}"
 ssh root@host01 "${all_commands}"
