66c6f89781aa11a4243cf34d8184f132f15f0340
Geoffrey Grosenbach <boss@topfunky.com>
2018-08-03 15:21:04 -0700

    Updated

--- consul-connect/courseBase.sh
+++ consul-connect/courseBase.sh
@@ -5,8 +5,5 @@ curl -L -o ~/consul.zip https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2
 unzip -d  ~/.bin/ ~/consul.zip
 rm ~/consul.zip
 
-mv ~/assets/bin/* ~/.bin
-chmod +x ~/.bin/*
-
-ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/consul.zip https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/consul.zip && rm ~/consul.zip && mv ~/assets/bin/* ~/.bin && chmod +x ~/.bin/*"
-ssh root@host01 "mkdir -p ~/log && nohup sh -c \"consul agent -dev -config-dir=~/assets/etc/consul.d >~/log/consul.log 2>&1 &\" &"
+ssh root@host01 'mkdir -p ~/log && nohup sh -c "consul agent -dev -config-dir=~/config >~/log/consul.log 2>&1 &" &'
+ssh root@host02 'echo "HELLO"'
