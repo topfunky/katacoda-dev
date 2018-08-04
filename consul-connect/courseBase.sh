e39749cdd007704bd8e1cbcd21f0077be5670d51
Geoffrey Grosenbach <boss@topfunky.com>
2018-08-03 22:50:21 -0700

    Updated

--- consul-connect/courseBase.sh
+++ consul-connect/courseBase.sh
@@ -7,16 +7,8 @@ host_commands=(
 "cd ~/src && curl -L https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip -O"
 "unzip -d /usr/local/bin ~/src/consul*.zip"
 
-"cd ~/src && curl -L https://github.com/topfunky/katacoda-dev/raw/master/consul-connect/assets/bin/counting-service -O"
-"mv ~/src/counting-service /usr/local/bin"
-"cd ~/src && curl -L https://github.com/topfunky/katacoda-dev/raw/master/consul-connect/assets/bin/dashboard-service -O"
-"mv ~/src/dashboard-service /usr/local/bin"
-"chmod +x /usr/local/bin/*-service"
-
 "useradd consul --create-home"
 "mkdir -p /etc/consul.d"
-"cd /etc/consul.d && curl -L https://github.com/topfunky/katacoda-dev/raw/master/consul-connect/assets/config/counting.json -O"
-"cd /etc/consul.d && curl -L https://github.com/topfunky/katacoda-dev/raw/master/consul-connect/assets/config/dashboard.json -O"
 "mkdir -p /home/consul/log"
 "chown -R consul /home/consul"
 "runuser -l consul -c \"consul agent -dev -config-dir=/etc/consul.d >/home/consul/log/consul.log 2>&1 &\""
