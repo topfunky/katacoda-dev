fd04dcf181f9cdbd776fde315eb50e760fa0df00
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2020-04-01 12:54:49 -0700

    Add main.tf code file

---
+++ terraform-docker/assets/main.tf
@@ -0,0 +1,11 @@
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
