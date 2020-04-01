92dc053efe5b09bc9e01a45f1198af6062566e66
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2020-04-01 12:48:59 -0700

    Use assets for code file

--- terraform-docker/courseBase.sh
+++ terraform-docker/courseBase.sh
@@ -1,15 +1,2 @@
 mkdir -p ~/terraform-docker-demo
-tee ~/terraform-docker-demo/main.tf <<EOF
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 80
-  }
-}
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-EOF
 cd ~/terraform-docker-demo
